37dfff1c082f058485b35427d8bf4f35deb2df99
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2023-02-07 21:28:12 +0900

    wip

--- bin/brew.sh
+++ bin/brew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "homebrewをインストールします..."
-which /opt/homebrew/bin/brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+./bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo ".Brewfileで管理しているアプリケーションをインストールします..."
-which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew bundle --file ../homebrew/Brewfile --verbose
+brew bundle --file ../homebrew/Brewfile --verbose
