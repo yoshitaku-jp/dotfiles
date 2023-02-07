af751f2fb9c06bec268d5c70a90b7cfba3dc6e90
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2023-02-07 21:21:54 +0900

    wip

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,7 +3,4 @@
 echo "Xcodeをインストールします..."
 xcode-select --install
 
-echo "homebrewをインストールします..."
-which /opt/homebrew/bin/brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 exec $SHELL -l
