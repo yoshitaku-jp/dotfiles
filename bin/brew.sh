fb27c68f819f3701661bafdebaa3c9c5b323dfdc
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2024-03-20 23:37:49 +0900

    not sudo

--- bin/brew.sh
+++ bin/brew.sh
@@ -2,12 +2,12 @@
 
 if ! command -v brew &> /dev/null; then
   echo "Homebrewがインストールされていません。"
-  echo "https://brew.sh/index_ja"
   echo "Homebrewをインストールしています..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "Homebrewをインストールしました"
 else
   echo "Homebrewがすでにインストールされています。"
 fi
 
 echo ".Brewfileで管理しているアプリケーションをインストールします..."
-sudo -n brew bundle --file ./homebrew/Brewfile --verbose
+brew bundle --file ./homebrew/Brewfile --verbose
