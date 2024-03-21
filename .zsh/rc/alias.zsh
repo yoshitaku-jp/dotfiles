46291cd6dd6960747e404cef0bfa10040620457d
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2024-03-21 20:27:43 +0900

    rm zshs

--- .zsh/rc/alias.zsh
+++
@@ -1,8 +0,0 @@
-alias ll='ls -alF'  # ls -alFを短縮形のllで実行
-alias la='ls -A'  # ls -Aを短縮形のlaで実行
-alias l='ls -CF'  # ls -CFを短縮形のlで実行
-alias c='clear'  # clearを短縮形のcで実行
-alias ..='cd ..'  # cd ..を短縮形の..で実行
-alias grep='grep --color=auto'  # grepを実行する際に、色付きの出力を有効にする
-alias vi='vim'  # viをvimで置き換える
-alias py='python'  # pythonを短縮形のpyで実行
