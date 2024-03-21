3464c22f61903249deb8e49622d47164cc5038da
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2024-03-21 22:59:13 +0900

    .

---
+++ .zsh/rc/base.zsh
@@ -0,0 +1,20 @@
+# 環境変数
+export LANG=ja_JP.UTF-8
+
+# ヒストリの設定
+HISTFILE=~/.zsh_history
+HISTSIZE=50000
+SAVEHIST=50000
+
+# 直前のコマンドの重複を削除
+setopt hist_ignore_dups
+
+# 同じコマンドをヒストリに残さない
+setopt hist_ignore_all_dups
+
+# 同時に起動したzshの間でヒストリを共有
+setopt share_history
+
+# ディレクトリスタック
+DIRSTACKSIZE=100
+setopt AUTO_PUSHD
